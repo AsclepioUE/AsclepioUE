7759915bd3bd9e2bddbcdaf8b301326db7253e3e
DanielFernandez <dfernandezlouro1@gmail.com>
2022-06-01 10:55:35 +0200

    Create main

---
+++ Asclepio_Project/main/inicioAplicacion.java
@@ -0,0 +1,23 @@
+package main;
+
+import java.awt.EventQueue;
+
+public class inicioAplicacion {
+
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			
+			@Override
+			public void run() {
+				// TODO Auto-generated method stub
+				
+				
+				
+				
+				
+			}
+		});
+
+	}
+
+}
